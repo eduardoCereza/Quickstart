e1883e0a74c77581a01235ff715ed4338ac3f8bd
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-06-03 13:43:06 -0300

    testes

---
+++ TeamCode/src/main/java/pre_temporada/PedroPathing_Teste_TeleOp.java
@@ -0,0 +1,15 @@
+package pre_temporada;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class PedroPathing_Teste_TeleOp extends OpMode {
+
+    @Override
+    public void init(){
+
+    }
+
+    public void loop(){
+        
+    }
+}
