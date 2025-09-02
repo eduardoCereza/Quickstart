302f7cd142de32781d59cf98a47f30b4edc7755a
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-09-02 18:15:53 -0300

    Trabalhando e Estudando sobre o PP novo

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Teleoperate_Mode.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class Teleoperate_Mode extends OpMode {
+
+    //TODO: Criação das variável
+    private Follower follower;
+
+    Servo garra, ponta;
+    DcMotor slide;
+    private Hw_Others hw;
+    double x, y, turn;
+    private final Pose startPose = new Pose(60, 96, Math.toRadians(180));
+
+    //TODO: Método de inicialização do período Teleoperado
+    @Override
+    public void init(){
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(startPose);
+        hw = new Hw_Others(garra, ponta, slide);
+        hw.init(hardwareMap);
+    }
+
+    //TODO: Método para começar o período Teleoperado
+    @Override
+    public void start(){
+        follower.startTeleopDrive();
+    }
+
+    //TODO: Método de loop infinito para o teleoperado
+    @Override
+    public void loop(){
+        x = gamepad1.left_stick_y;
+        y = gamepad1.left_stick_x;
+        turn = gamepad1.right_stick_x;
+
+        follower.setTeleOpDrive(x, y, turn, false);
+        follower.update();
+    }
+}
