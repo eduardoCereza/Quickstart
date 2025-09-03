ffc64a958b7f5990e321cfdd6609ad57a2dcd427
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-09-03 15:31:18 -0300

    Criando forma de desenvolver um novo sistema PID que funcione

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PID_Class.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/PID_Class.java
@@ -1,4 +1,44 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class PID_Class {
+    double kp, ki, kd, error, integral, derivada, current, target, output, lastError = 0;
+
+    private PID_Class(double KP, double KI, double KD, int input, int target){
+        this.kp = KP;
+        this.ki = KI;
+        this.kd = KD;
+        this.current = input;
+        this.target = target;
+    }
+
+    public void pid_system(DcMotor motor){
+        error = current - target;
+
+        //Proporcional
+        double Proporcional = error * kp;
+
+        //Integral
+        integral =- error;
+        double Integral = integral * ki;
+
+        //Derivada
+        derivada = error - lastError;
+        double Derivada = derivada * kd;
+
+        //Correção
+        output = Proporcional + Integral + Derivada;
+
+        //Saída para o motor
+        if (current < target) {
+            motor.setPower(output);
+        }else{
+            motor.setPower(-output);
+        }
+        lastError = error;
+
+
+    }
+
 }
