b20895333f0abd4f69e7bd125db1e537677a1d3c
Eduardo Cereza <eduardo.cereza@outlook.com>
2025-09-19 13:58:00 -0300

    Testando limelight

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Prototipos/Limelight3A_Teste.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Prototipos/Limelight3A_Teste.java
@@ -1,12 +1,18 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.Prototipos;
 
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import java.util.List;
+
 
 @TeleOp(name = "Teste Limelight", group = "Prototipos")
 public class Limelight3A_Teste extends OpMode {
+
+    int[] id = {21, 22, 23};
     Limelight3A limelight;
 
     @Override
@@ -18,12 +24,28 @@ public class Limelight3A_Teste extends OpMode {
         limelight.start();
 
         telemetry.addData(">", "Robot Ready.  Press Play.");
-        telemetry.update();
+
+
 
     }
 
     @Override
     public void loop() {
+        LLResult result = limelight.getLatestResult();
+        // Access fiducial results
+        List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+        for (LLResultTypes.FiducialResult fr : fiducialResults) {
+
+            if(fr.getFiducialId() == id[0]){
+                telemetry.addLine("Ordem GPP");
+            } else if (fr.getFiducialId() == id[1]) {
+                telemetry.addLine("Ordem PGP");
+            } else if (fr.getFiducialId() == id[2]) {
+                telemetry.addLine("Ordem PPG");
+            }
+        }
+        telemetry.update();
+
 
     }
 }
